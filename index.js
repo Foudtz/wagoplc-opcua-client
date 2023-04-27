9742bf6cd18fd4496ac252317c5bd65c2fdddbaf
Foudtz <108090081+Foudtz@users.noreply.github.com>
2023-04-27 17:35:13 +0200

    Update class

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 // Requires: node-opcua, winston
-const opcua = require("node-opcua-nodeid");
-const { _enumerationDataChangeTrigger } = require("node-opcua-types");
-const {
-	OPCUAClient,
-	AttributeIds,
-	ClientSubscription,
-	TimestampsToReturn,
-	MessageSecurityMode,
-	SecurityPolicy,
-	MonitoringMode,
-	BrowseDirection
-} = require("node-opcua");
-
+const opcua = require("node-opcua");
+const fs = require("fs");
 const { createLogger, format, transports } = require("winston");
- 
-// REquire printf
 const { printf, combine, timestamp, label, prettyPrint } = format;
-
-const logLevels = {
-  fatal: 0,
-  error: 1,
-  warn: 2,
-  info: 3,
-  debug: 4,
-  trace: 5,
-};
-
-// Logger format
+const logLevels = {  fatal: 0,  error: 1,  warn: 2,  info: 3,  debug: 4,  trace: 5, };
 const loggerFormat = printf(({ level, message, label, timestamp }) => {
 	return `${timestamp} [${label}] ${level}: ${message}`;
 });
@@ -57,15 +34,20 @@ const logger = createLogger({
 //
 class WagoPLC{
 	// Wago variables
-	ip = null;							// IP address of the PLC
-	wLogin = null;						// Login of the PLC
-	password = null;					// Password of the PLC
-	session = null;						// Session with the PLC
-	subscription = null;				// Subscription to the PLC
-	itemsToMonitor = [];				// Array of items to monitor
-	itemsAvailable = [];				// Array of items browsed from the PLC
-	client = null;						// OPCUA client
-	
+	ip = null;									// IP address of the PLC
+	wLogin = null;								// Login of the PLC
+	password = null;							// Password of the PLC
+	session = null;								// Session with the PLC
+	subscription = null;						// Subscription to the PLC
+	itemsToMonitor = [];						// Array of items to monitor
+	itemsAvailable = [];						// Array of items browsed from the PLC
+	client = null;								// OPCUA client
+	SocketIO = null;							// Socket to send data to the frontends
+
+	// Static variables
+	cacheDirectory = "./cache";					// Cache directory for the OPCUA client
+	cacheFile = "items.json";					// File to store the items browsed from the PLC
+
 	// Others variables
 	debug = false;						// Log events
 	currentBrowsing = false;			// True if a browsing is in progress
@@ -73,15 +55,21 @@ class WagoPLC{
 	// Constructor with the IP address of the PLC, login and password
 	constructor(ip, login, password){
         logger.info("=== WagoPLC constructor on : " + ip + " ===");
+	
+		// Check if cache directory exists
+		if (!fs.existsSync(this.cacheDirectory)){
+			fs.mkdirSync(this.cacheDirectory);
+		}
 
-		this.debug = true;					// Log events
+		// Delete cache file if exist
+		this.#deleteCacheFile();
 
 		this.ip = ip;
 		this.wLogin = login;
 		this.password = password;
 
 		// Create OPCUA client
-		this.client = OPCUAClient.create({
+		this.client = opcua.OPCUAClient.create({
 			connectionStrategy:{ initialDelay: 1000, maxRetry: 1},
 			endpointMustExist: true
 		});
@@ -93,7 +81,6 @@ class WagoPLC{
 		this.client.on("close", function(){	                        	logger.info("  |_ Close !")} );
 		this.client.on("start_reconnection", function(){	        	logger.info("  |_ Start reconnection !")} );
 		this.client.on("after_reconnection", function(){	        	logger.info("  |_ Reconnection OK !")} );
-	
 		this.client.on("backoff", function(){	                    	logger.debug("  |_ Backoff !")} );
 		this.client.on("reconnection_attempt_has_failed", function(){	logger.debug("  |_ Reconnection attempt has failed !")} );
 		this.client.on("abort", function(){	                        	logger.debug("  |_ Abort !")} );
@@ -105,12 +92,29 @@ class WagoPLC{
 		this.client.on("security_token_renewed", function(){	    	logger.debug("  |_ Security token renewed !")} );
 		this.client.on("connection_reestablished", function(){	    	logger.debug("  |_ Connection reestablished !")} );
 		this.client.on("timed_out_request", function(){	            	logger.debug("  |_ Timed out request !")} );
-
 		logger.info("  |_ WagoPLC constructor done !");
 	}
 
-	logLevel(level){
+	setLogLevel(level){
 		logger.level = level;
+	}
+
+	setCacheDirectory(directory){
+		this.cacheDirectory = directory;
+	}
+
+	setCacheFile(file){
+		this.cacheFile = file;
+	}
+
+	ping(){
+		const ping = this.session !== null && this.session.sessionId !== null;
+		logger.debug("  |_ Ping : " + ping);
+		return ping;
+	}
+
+	setSocketIO(io){
+		this.SocketIO = io;
 	}
 
 	// Connect to the PLC with ip, create the session with login and password
@@ -146,42 +150,180 @@ class WagoPLC{
 		if(this.subscription != null)	await this.subscription.terminate();
 	}
 
+	#logItem(who){
+		// Get Item from the monitored list
+		const item = this.FindMonitoredItem(who);
+		if(item === false) 		return;
+
+		console.log(item);
+	}
+
 	// Read data from the PLC. Return the value of the data
 	async readData(nodeId){
 		// Read the data
 		const dataValue = await this.session.read({
 			nodeId: nodeId,
-			attributeId: AttributeIds.Value
+			attributeId: opcua.AttributeIds.Value
 		});
 
 		// Log the value
-		logger.info("    |_ Read data : " + nodeId + " = " + dataValue.value);
-		
-		return dataValue.value.value;
+		logger.info("    |_ Read data : " + nodeId);
+
+		return dataValue.value;
+	}
+
+	// Read data from the PLC. Return the value of the data
+	async readDataType(nodeId){
+		// Read the data
+		const dataValue = await this.session.read({
+			nodeId: nodeId,
+			attributeId:opcua.AttributeIds.DataType
+		});
+
+		// Log the value
+		logger.info("    |_ Read type : " + nodeId);
+
+		return dataValue.value;
+	}
+
+	FindAvailableItem(who){
+		who = who.toString();
+		const item = this.itemsAvailable.find(item => item.node.nodeId == who);
+
+		return item;
+	}
+
+	FindAvailableItemIndex(who){
+		who = who.toString();
+		const index = this.itemsAvailable.findIndex(item => item.node.nodeId == who);
+
+		return index;
+	}
+
+	FindMonitoredItem(who){
+		// Search item on monitor list. If not monitored, we should be allowed to modify it.
+		who = who.toString();
+		const item = this.itemsToMonitor.find(item => item.nodeId == who);
+
+		return item;
+	}
+
+	async switchBoolValue(who){
+		// Get Item from the monitored list
+		const item = this.FindMonitoredItem(who);
+		if(item === false) 		return;
+
+		return this.writeData(who, !item.value);
+	}
+
+	// Write data to the PLC. Return the status code
+	async writeData(who, what){
+		// Get Item from the monitored list
+		const item = this.FindMonitoredItem(who);
+		if(item === false) 		return;
+
+		var WriteValue = null;
+		var WriteDataType = null;
+
+		logger.info("    |_ Write data : " + who + " : " + what);
+
+		switch (typeof item.value) {
+			case "boolean":
+				WriteValue 		= what;
+				WriteDataType 	= opcua.DataType.Boolean;
+				break;
+
+			case "object":
+				// Construct new value by merging the structure of the old value with the new value
+				WriteValue 		= Object.assign(item.value, what);
+				// Construct the ExtensionObject
+				WriteValue 		= await this.session.constructExtensionObject(item.dataType, WriteValue);
+				WriteDataType 	= opcua.DataType.ExtensionObject
+				break;
+
+			case "number":
+				if( item.dataType.value == 10 ){
+					WriteValue		= Number.parseFloat(what);
+					WriteDataType 	= opcua.DataType.Float;
+				}else if( item.dataType.value == 4 ){
+					WriteValue		= Number.parseInt(what);
+					WriteDataType 	= opcua.DataType.Int16;
+				}else if( item.dataType.value == 6 ){
+					WriteValue		= Number.parseInt(what);
+					WriteDataType 	= opcua.DataType.Int32;
+				}else if( item.dataType.value == 8 ){
+					WriteValue		= Number.parseInt(what);
+					WriteDataType 	= opcua.DataType.Int64;
+				}
+				break;
+
+			default:
+				logger.error("    |_ Unknown data type : " + typeof item.value);
+				return false;
+		}
+
+		logger.info("      |_ Value : " + JSON.stringify(WriteValue) );
+
+		// Write to the PLC
+		return await this.session.write({
+			nodeId: item.nodeId,
+			attributeId: opcua.AttributeIds.Value,
+			value: new opcua.DataValue({
+				value: new opcua.Variant({
+					value: WriteValue,
+					dataType: WriteDataType
+				})
+			})
+		});
 	}
 
 	// Add item to monitor
 	async addItemToMonitor(nodeId, callback){
+		//Check if node is already monitored
+		const item = this.FindMonitoredItem(nodeId);
+		if(item !== undefined){
+			logger.info("  |_ Item already monitored : " + nodeId);
+			console.log(item);
+			return;
+		}
+
+		const dataTypeId = await this.readDataType(nodeId);
+		const dataType = opcua.coerceNodeId(dataTypeId.value);
+		const values = await this.readData(nodeId);
+
 		// Add item to monitor
 		this.itemsToMonitor.push({
 			nodeId: nodeId,
-			attributeId: AttributeIds.Value,
+			attributeId: opcua.AttributeIds.Value,
 			indexRange: null,
-			dataEncoding: { namespaceIndex: 0, name: null }
+			dataEncoding: { namespaceIndex: 0, name: null },
+			dataTypeId : dataTypeId.value,
+			dataType : dataType,
+			value : values.value
 		});
+
 		// Log the item	
 		logger.info("  |_ Add item to monitor : " + nodeId);
 	}
 
+	getMonitoredItems(){
+		return this.monitoredItems;
+	}
+
 	// Start monitoring data changes
 	async startMonitoringDataChanges(){
+		if( this.itemsToMonitor.length == 0 ){
+			logger.info("  |_ No items to monitor (" + this.itemsToMonitor.length + ") !");
+			return;
+		}
+
 		// Create a subscription
-		logger.info("  |_ Create Subscription / Monitor items !")
+		logger.info("  |_ Create Subscription / Monitor items (" + this.itemsToMonitor.length + ") !")
 
 		this.subscription = await this.session.createSubscription2({
 			requestedPublishingInterval: 1000,
 			requestedLifetimeCount: 100,
-			requestedMaxKeepAliveCount: 10,
+			requestedMaxKeepAliveCount: 30,
 			maxNotificationsPerPublish: 10,
 			publishingEnabled: true,
 			priority: 10
@@ -189,7 +331,7 @@ class WagoPLC{
 
 		// Log events
 		this.subscription.on("started", function(){	            logger.info("  |_Subscription started !")} );
-		this.subscription.on("keepalive", function(){	        logger.info("  |_Subscription keep alive : " + new Dateé().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }) )} );
+		this.subscription.on("keepalive", function(){	        logger.info("  |_Subscription keep alive : " + new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }) )} );
 		this.subscription.on("terminated", function(){	        logger.info("  |_Subscription terminated !")} );
 		this.subscription.on("internal_error", function(){	    logger.info("  |_Subscription internal error !")} );
 		this.subscription.on("status_changed", function(){	    logger.info("  |_Subscription status changed !")} );
@@ -202,7 +344,7 @@ class WagoPLC{
 			discardOldest: true,
 
 			queueSize: 10
-		}, TimestampsToReturn.Both, (err, monitoredItems) => {
+		}, opcua.TimestampsToReturn.Both, (err, monitoredItems) => {
 			if(err) logger.info(err);
 
 			// Log events
@@ -210,45 +352,141 @@ class WagoPLC{
 			monitoredItems.on("terminated", function(){	        logger.info("    |_Monitored items terminated !")} );
 
 			monitoredItems.on("changed", (events, dataValue, index) => {
-				if( dataValue.value.value.nom && monitoredItemsValues[index] != dataValue.value.value.export_state ){
-					logger.info("    |_ Item changed [" + index + "] " + dataValue.value.value.nom + " : " + dataValue.value.value.export_state);
-					monitoredItemsValues[index] = dataValue.value.value.export_state;
+				logger.info("    |_ Item changed [" + index + "] " + events.itemToMonitor.nodeId );
+				logger.info("      |_ Value : " + JSON.stringify(dataValue.value.value) );
+
+				if( this.SocketIO && this.SocketEmitEvent ){
+					this.SocketIO.sockets.emit('itemChanged', { nodeId: events.itemToMonitor.nodeId, value: dataValue.value.value } );
 				}
+
+				const item = {
+					data: dataValue.value.value,
+					index: index
+				}
+				return item;
 			});
 		});
 	}
 
-    // Function schedule browse every 10 seconds
-    scheduleBrowse(rootNode){
-        // Browse the data
-        this.browse(rootNode);
-
-        // Schedule the next browse
-        setTimeout(function(){ this.browse(rootNode) }, 10000);
-    }
-
-	async browse(rootNode){
-        // Reset item available
-        this.itemsAvailable = [];
-
-		logger.info("  |_ Browsing root on : " + rootNode);
-		this.browseNodeRecursiv( rootNode );
+	#getCacheFilePath(){
+		return this.cacheDirectory + '/' + this.cacheFile;
 	}
 
-	async browseNodeRecursiv(nodeId, level = 0){
+	// Delete cache file
+	async #deleteCacheFile(){
+		const itemFile = this.#getCacheFilePath();
+
+		if( !fs.existsSync(itemFile) ) return;
+
+		logger.info("  |_ Delete cache file : " + itemFile);
+
+		fs.unlink(itemFile, function(err) {
+			if(err) 			logger.error("    |_ Delete cache file : KO | " + err);
+			else				logger.info("    |_ Delete cache file : OK");
+		});
+	}
+
+	// Write items to cache file
+	async #writeItemsToCacheFile(){
+		const itemFile = process.env.CACHE_DIR + '/' + process.env.CACHE_ITEMS_FILE;
+		logger.info("  |_ Write cache file : " + itemFile);
+
+		// Write items to file
+		fs.writeFile(itemFile, JSON.stringify(this.itemsAvailable, null, 2), function(err) {
+			if(err) 			logger.error("    |_ Write cache file : KO | " + err);
+			else				logger.info("    |_ Write cache file : OK");
+		});
+	}
+
+	getItemsAvailable(){
+		return this.itemsAvailable();
+	}
+
+	// Get items from cache file
+	async getItemsFromCacheFile(){
+		if( this.currentBrowsing )		return 5;
+		
+		const itemFile = process.env.CACHE_DIR + '/' + process.env.CACHE_ITEMS_FILE;
+
+		logger.info("  |_ Read cache file : " + itemFile);
+
+		// Check if the file exists
+		if( !fs.existsSync(itemFile) ){
+			logger.error("    |_ File doest not exist : " + itemFile);
+			return 6;
+		}
+
+		// Read the file
+		try {
+			const data = fs.readFileSync(itemFile, 'utf8')
+			this.itemsAvailable = JSON.parse(data);
+			logger.info("    |_ Read cache file : OK");
+		} catch (err) {
+			logger.error("    |_ Read cache file : KO | " + err);
+		}
+
+		return this.itemsAvailable;
+	}
+
+	async browse(rootNode, monitor=false){
+		logger.info("  |_ Browsing root on : " + rootNode);
+		await this.#browseNodeRecursiv( rootNode, 0, monitor ).then( () => {
+			logger.info("    |_ Browsing OK, cache values");
+			
+			this.#writeItemsToCacheFile();
+			this.currentBrowsing = false;
+		});
+	}
+
+	async #browseNodeRecursiv(nodeId, level = 0, monitor=false){
 		this.currentBrowsing = true;
 		// Browse the data
 		const browseResult = await this.session.browse(nodeId);
 
 		// Log all the values
 		for(var i = 0; i < browseResult.references.length; i++){
-			// Save in itemsAvailable
-			this.itemsAvailable[browseResult.references[i].nodeId.value] = browseResult.references[i];
+			// Read data and type of the node
+			const dataTypeId = await this.readDataType(browseResult.references[i].nodeId);
+			const dataType = opcua.coerceNodeId(dataTypeId.value);
+			const values = await this.readData(browseResult.references[i].nodeId);
+
+			const index = this.FindAvailableItemIndex(browseResult.references[i].nodeId);
+			if( index != -1 ){
+				// Update item
+				this.itemsAvailable[index] = {
+					node: browseResult.references[i],
+					dataTypeId : dataTypeId.value,
+					dataType : dataType,
+					value : values.value
+				}
+			}else{
+				// Add item to monitor
+				this.itemsAvailable.push({
+					node: browseResult.references[i],
+					dataTypeId : dataTypeId.value,
+					dataType : dataType,
+					value : values.value
+				});
+
+				// Monitor the item if needed
+				if( monitor ){
+					this.addItemToMonitor(browseResult.references[i].nodeId);
+				}
+			}
 
 			logger.debug(" ".repeat(level) + "    |_ Browse : " + browseResult.references[i].nodeId + " / " + browseResult.references[i].browseName.name + " (" + browseResult.references[i].nodeId.value + ")");
-			await this.browseNodeRecursiv(browseResult.references[i].nodeId, (level + 1));
+			await this.#browseNodeRecursiv(browseResult.references[i].nodeId, (level + 1));
 		}
-		this.currentBrowsing = false;
+	}
+
+	searchItemByNameLike(name){
+		var result = [];
+		for(var key in this.itemsAvailable){
+			if(this.itemsAvailable[key].browseName.name.toLowerCase().includes(name.toLowerCase())){
+				result.push(this.itemsAvailable[key]);
+			}
+		}
+		return result;
 	}
 }
 
